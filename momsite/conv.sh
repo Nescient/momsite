a3df881269bf8b24b9190575459f91146ce810a0
go away <spam@spam.spam>
2016-11-13 15:45:13 -0500

    cleanup
    cleaned the conversion script

--- momsite/conv.sh
+++ momsite/conv.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
+#Copyright © Sam Savage 2016
 
+MAKE_DIRS=true
+DO_ALL=true
+DO_WATERMARK=false
 
-for i in $(seq 1 23); do
-   convert -density 100 -depth 2 mark/"$i"_w1.png mark2/"$i"_w1.png
-   convert -density 100 -depth 2 mark/"$i"_w9.png mark2/"$i"_w9.png
-   convert -density 100 -depth 2 mark/"$i"_w8.png mark2/"$i"_w8.png
-   convert -density 100 -depth 2 mark/"$i"_w7.png mark2/"$i"_w7.png
-   convert -density 100 -depth 2 mark/"$i"_w6.png mark2/"$i"_w6.png
+while getopts "di:w" opt; do
+   case $opt in
+      d) MAKE_DIRS=false;;
+      i) DO_ALL="$OPTARG";;
+      w) DO_WATERMARK=true;;
+  esac
 done
 
-exit
+if [ "$DO_WATERMARK" == true ]; then
+   convert -trim -density 600 test.pdf a.png
+   convert a.png -fuzz 20% -transparent white -resize 3700x3700 -trim b.png
+   convert b.png -density 100 -depth 2 watermark.png
+   convert watermark.png -resize 90% watermark90.png
+   convert watermark.png -resize 80% watermark80.png
+   convert watermark.png -resize 70% watermark70.png
+   convert watermark.png -resize 60% watermark60.png
+fi
 
-mkdir raw
-mkdir norm
-mkdir mark
+if [ "$MAKE_DIRS" == true ]; then
+   mkdir raw
+   mkdir norm
+   mkdir mark
+fi
 
-# for i in $(ls *.pdf); do
-#    echo $i to "$i.png"
-# done
+SEQ=$(seq 1 23)
 
-for i in $(seq 1 23); do
+if [ "$DO_ALL" != true ]; then
+   SEQ=$DO_ALL
+fi
+
+for i in $SEQ; do
    echo "Image $i"
    convert -trim -density 600 -flatten $i.pdf raw/$i.png
    convert -density 100 -resize 3700x3700 -depth 2 -trim raw/$i.png norm/$i.png
@@ -29,67 +45,3 @@ for i in $(seq 1 23); do
    composite -gravity center watermark70.png norm/$i.png mark/"$i"_w7.png
    composite -gravity center watermark60.png norm/$i.png mark/"$i"_w6.png
 done
-
-exit
-
-convert -trim -density 600 -resize 3700x3700 -flatten 1.pdf 1.png
-convert -trim -density 600 -resize 3700x3700 -flatten 2.pdf 2.png
-convert -trim -density 600 -resize 3700x3700 -flatten 3.pdf 3.png
-convert -trim -density 600 -resize 3700x3700 -flatten 4.pdf 4.png
-convert -trim -density 600 -resize 3700x3700 -flatten 5.pdf 5.png
-convert -trim -density 600 -resize 3700x3700 -flatten 6.pdf 6.png
-convert -trim -density 600 -resize 3700x3700 -flatten 7.pdf 7.png
-convert -trim -density 600 -resize 3700x3700 -flatten 8.pdf 8.png
-convert -trim -density 600 -resize 3700x3700 -flatten 9.pdf 9.png
-convert -trim -density 600 -resize 3700x3700 -flatten 10.pdf 10.png
-convert -trim -density 600 -resize 3700x3700 -flatten 11.pdf 11.png
-convert -trim -density 600 -resize 3700x3700 -flatten 12.pdf 12.png
-convert -trim -density 600 -resize 3700x3700 -flatten 13.pdf 13.png
-convert -trim -density 600 -resize 3700x3700 -flatten 14.pdf 14.png
-convert -trim -density 600 -resize 3700x3700 -flatten 15.pdf 15.png
-convert -trim -density 600 -resize 3700x3700 -flatten 16.pdf 16.png
-convert -trim -density 600 -resize 3700x3700 -flatten 17.pdf 17.png
-convert -trim -density 600 -resize 3700x3700 -flatten 18.pdf 18.png
-convert -trim -density 600 -resize 3700x3700 -flatten 19.pdf 19.png
-convert -trim -density 600 -resize 3700x3700 -flatten 20.pdf 20.png
-convert -trim -density 600 -resize 3700x3700 -background white -alpha remove 21.pdf 21.png
-convert -trim -density 600 -resize 3700x3700 -flatten 22.pdf 22.png
-convert -trim -density 600 -resize 3700x3700 -flatten 23.pdf 23.png
-
-exit
-
-composite -gravity center watermark70.png 1.png 1w.png
-composite -gravity center watermark.png 2.png 2w.png
-composite -gravity center watermark.png 3.png 3w.png
-composite -gravity center watermark.png 4.png 4w.png
-composite -gravity center watermark.png 5.png 5w.png
-composite -gravity center watermark.png 6.png 6w.png
-composite -gravity center watermark.png 7.png 7w.png
-composite -gravity center watermark.png 8.png 8w.png
-composite -gravity center watermark.png 9.png 9w.png
-composite -gravity center watermark.png 10.png 10w.png
-composite -gravity center watermark.png 11.png 11w.png
-composite -gravity center watermark.png 12.png 12w.png
-composite -gravity center watermark.png 13.png 13w.png
-composite -gravity center watermark.png 14.png 14w.png
-composite -gravity center watermark.png 15.png 15w.png
-composite -gravity center watermark.png 16.png 16w.png
-composite -gravity center watermark.png 17.png 17w.png
-composite -gravity center watermark.png 18.png 18w.png
-composite -gravity center watermark.png 19.png 19w.png
-composite -gravity center watermark.png 20.png 20w.png
-composite -gravity center watermark.png 21.png 21w.png
-composite -gravity center watermark.png 22.png 22w.png
-composite -gravity center watermark.png 23.png 23w.png
-
-exit
-
-convert -density 600 -size 3700x3700 "$1" "$1.png"
-composite -gravity center "$1.png" "$1_watermark.png"
-exit
-
-convert -density 600 -resize 3700x3700 test.pdf a.png
-convert a.png -fuzz 20% -transparent white watermark.png
-
-convert watermark.png -resize 50% watermark50.png
-
